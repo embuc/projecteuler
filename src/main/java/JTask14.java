d9aa4e7ed01c4f8d4c83e90b395e8fb8af1bb6fb
Emir Bucalovic <emir.bucalovic@tactel.se>
2023-12-28 17:22:25 +0100

    Task 14 done.

---
+++ src/main/java/JTask14.java
@@ -0,0 +1,37 @@
+import se.embuc.Task;
+
+//Longest Collatz Sequence
+public class JTask14 implements Task {
+
+	private int maxNumber = 1_000_000;
+	private long [] cache = new long[maxNumber];
+
+	@Override
+	public Object solve() {
+		var maxLength = 0L;
+		var number = 0L;
+
+		for (int i = 1; i < 1_000_000; i++) {
+			var length = findColatzSeqLength(i);
+			if (length > maxLength) {
+				maxLength = length;
+				number = i;
+			}
+		}
+		return number;
+	}
+
+	long findColatzSeqLength(long n){
+		if (n < maxNumber && cache[(int)n] != 0L) {
+			return cache[(int)n];
+		}
+
+		var next = (n % 2 == 0L) ? n >> 1 : 3 * n + 1;
+		var length = (n != 1L) ? 1 + findColatzSeqLength(next) : 1;
+		if (n < maxNumber) {
+			cache[(int)n] = length;
+		}
+
+		return length;
+	}
+}
---
+++ src/test/kotlin/Task14Test.kt
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import se.embuc.Task14
+
+class Task14Test {
+
+	@Test
+	fun solve() {
+		assertEquals(837799L, Task14().solve())
+		assertEquals(837799L, JTask14().solve())
+	}
+
+	@Test
+	fun findColatzSeqLength() {
+		assertEquals(10, Task14().findColatzSeqLength(13))
+	}
+}
