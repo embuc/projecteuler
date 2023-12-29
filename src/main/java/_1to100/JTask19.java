f59781cc3ddc4bacdbdbcea8cc64a86e6a6d7355
Emir Bucalovic <emir.bucalovic@tactel.se>
2023-12-29 12:13:28 +0100

    Task 20 done.

---
+++ src/main/java/_1to100/JTask19.java
@@ -0,0 +1,31 @@
+package _1to100;
+
+import se.embuc.Task;
+
+import java.util.Arrays;
+import java.util.List;
+
+// Counting Sundays
+public class JTask19 implements  Task {
+	private static final List<Integer> monthsInNonLeapYear = Arrays.asList(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
+	private static final List<Integer> monthsInLeapYear = Arrays.asList(31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
+	private static final List<String> days = Arrays.asList("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday");
+
+	@Override
+	public Object solve() {
+		int dayIndex = (monthsInNonLeapYear.stream().mapToInt(Integer::intValue).sum() + 1) % 7; // 1 Jan 1901 was a Tuesday
+		int sundayCounter = 0;
+
+		for (int year = 1901; year <= 2000; year++) {
+			boolean isLeap = year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+			List<Integer> monthsInYear = isLeap ? monthsInLeapYear : monthsInNonLeapYear;
+			for (int daysInMonth : monthsInYear) {
+				if ("Sunday".equals(days.get(dayIndex))) {
+					sundayCounter++;
+				}
+				dayIndex = (dayIndex + daysInMonth) % 7;
+			}
+		}
+		return sundayCounter;
+	}
+}
---
+++ src/test/kotlin/_1to100/Task19Test.kt
@@ -0,0 +1,14 @@
+package _1to100
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import se.embuc._1to100.Task19
+
+class Task19Test {
+
+	@Test
+	fun solve() {
+		assertEquals(171, Task19().solve())
+		assertEquals(171, JTask19().solve())
+	}
+}
