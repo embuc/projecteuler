f8ca3c33b20dfb26d4e86837f93cd9e7e15c6c54
Emir Bucalovic <emir.bucalovic@tactel.se>
2024-01-01 14:47:53 +0100

    Task 29 done.

---
+++ src/main/kotlin/_1to100/Task29.kt
@@ -0,0 +1,17 @@
+package se.embuc._1to100
+
+import se.embuc.Task
+import java.math.BigInteger
+
+// Distinct powers
+class Task29:Task {
+	override fun solve(): Any {
+		val seq = mutableSetOf<BigInteger>()
+		for(a in 2..100) {
+			for (b in 2..100) {
+				seq.add(BigInteger.valueOf(a.toLong()).pow(b))
+			}
+		}
+		return seq.size
+	}
+}
---
+++ src/test/kotlin/_1to100/Task29Test.kt
@@ -0,0 +1,14 @@
+package _1to100
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import se.embuc._1to100.Task29
+
+class Task29Test {
+
+	@Test
+	fun solve() {
+		assertEquals(9183, Task29().solve())
+		assertEquals(9183, JTask29().solve())
+	}
+}
