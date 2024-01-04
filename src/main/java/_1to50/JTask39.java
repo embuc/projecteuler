5f16a6dac72d0f5470b206622d735b725528b60b
Emir Bucalovic <emir.bucalovic@tactel.se>
2024-01-04 19:46:19 +0100

    Task 39 optimized.

--- src/main/java/_1to50/JTask39.java
+++ src/main/java/_1to50/JTask39.java
@@ -5,23 +5,21 @@ import se.embuc.Task;
 public class JTask39 implements Task {
 	@Override
 	public Object solve() {
-		int max = 0;
-		int maxP = 0;
-		for (int p = 1; p <= 1000; p++) {
-			int count = 0;
-			for (int a = 1; a < p; a++) {
-				for (int b = a; b < p; b++) {
-					int c = p - a - b;
-					if (a * a + b * b == c * c) {
-						count++;
-					}
+		var maxSolutions = 0;
+		var perimeter = 0;
+
+		for (int p = 3; p < 1000; p++) {
+			var solutions = 0;
+			for (int a = 2; a < (p / 3); a++) {
+				if ((p * p - 2 * p * a) % (2 * p - 2 * a) == 0) {
+					solutions += 1;
 				}
 			}
-			if (count > max) {
-				max = count;
-				maxP = p;
+			if (solutions > maxSolutions) {
+				maxSolutions = solutions;
+				perimeter = p;
 			}
 		}
-		return maxP;
+		return perimeter;
 	}
 }
--- src/main/kotlin/_1to50/Task39.kt
+++ src/main/kotlin/_1to50/Task39.kt
@@ -2,25 +2,37 @@ package se.embuc._1to50
 
 import se.embuc.Task
 
-class Task39 :Task {
+/**
+ * Integer right triangles
+ * We know that a+b>c, otherwise the edges would not join to form a triangle. Adding c to both sides gives, a+b+c>2c,
+ * and as a+b+c<=1000, 2c<=1000, hence c<=500. Because c is the longest side, a<500, so it is only necessary for the
+ * loop to run so far.
+ * Further, With a,b,c the triangle sides, we have
+ * a+b+c == p
+ * a^2 + b^2 == c^2
+ *
+ * Combining these, we get
+ * b = (p^2 - 2(p*a)) / 2(p-a)
+ * Thus if (p^2 - 2(p*a)) % 2(p-a) == 0 we have a solution
+ *
+ */
+class Task39 : Task {
 	override fun solve(): Any {
-		var max = 0
-		var maxP = 0
-		for (p in 1..1000) {
-			var count = 0
-			for (a in 1 until p) {
-				for (b in a until p) {
-					val c = p - a - b
-					if (a * a + b * b == c * c) {
-						count++
-					}
+		var maxSolutions = 0
+		var perimeter = 0
+		for (p in 3 until 1000) {
+			var solutions = 0
+			for (a in 2..(p / 3)) {
+				if ((p * p - 2 * p * a) % (2 * p - 2 * a) == 0) {
+					solutions += 1
 				}
 			}
-			if (count > max) {
-				max = count
-				maxP = p
+			if (solutions > maxSolutions) {
+				maxSolutions = solutions
+				perimeter = p
 			}
 		}
-		return maxP
+		return perimeter
 	}
+
 }
