17b4a4247ee986a5791472f29f7578662a4392ab
Emir Bucalovic <emir.bucalovic@tactel.se>
2024-12-19 16:26:02 +0100

    Added extended logging for tests.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+
 plugins {
     kotlin("jvm") version "1.9.21"
     // Apply the Lombok plugin
@@ -15,6 +17,7 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.1")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.1")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.1")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.1")
     implementation("org.projectlombok:lombok:1.18.24")
@@ -25,6 +28,14 @@ dependencies {
 tasks{
     test {
         useJUnitPlatform()
+        // Enable detailed test output
+        testLogging {
+            events("passed", "skipped", "failed") // Log when tests pass, skip, or fail
+            exceptionFormat = TestExceptionFormat.SHORT // Use SHORT or FULL here
+            showCauses = true // Display root causes
+            showExceptions = true // Show exception stack traces
+            showStandardStreams = true // Show stdout/stderr output of tests
+        }
     }
 }
 kotlin {
